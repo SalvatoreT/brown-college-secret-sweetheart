460365456f4ce60cd51fad2b8c802fad8df3bb0a
Salvatore Testa <sal.testa@gmail.com>
2014-02-14 01:15:56 -0600

    finish rough draft

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+$(document).ready(function() {
+    var scrollorama = $.scrollorama({    	
+        blocks:'.scrollblock',
+        enablePin: false
+    });
+    var HEIGHT = $(window).height();
+    var WIDTH = $(window).width();
 
+	scrollorama
+		.animate('#left-buble1',{ delay: -HEIGHT, duration: HEIGHT/2, property:'left', start:-WIDTH/2 })
+		.animate('#left-buble1',{ delay: -HEIGHT, duration: HEIGHT/2, property:'opacity', start:0 });
+	scrollorama
+		.animate('#right-buble1',{ delay: -HEIGHT, duration: HEIGHT/2, property:'left', start:WIDTH/2 })
+		.animate('#right-buble1',{ delay: -HEIGHT, duration: HEIGHT/2, property:'opacity', start:0 });
+
+	scrollorama
+		.animate('#left-buble2',{ delay: 0, duration: HEIGHT/2, property:'left', start:-WIDTH/2 })
+		.animate('#left-buble2',{ delay: 0, duration: HEIGHT/2, property:'opacity', start:0 });
+	scrollorama
+		.animate('#right-buble2',{ delay: 0, duration: HEIGHT/2, property:'left', start:WIDTH/2 })
+		.animate('#right-buble2',{ delay: 0, duration: HEIGHT/2, property:'opacity', start:0 });
+
+	scrollorama
+		.animate('#left-buble3',{ delay: HEIGHT/4, duration: HEIGHT/2, property:'left', start:-WIDTH/2 })
+		.animate('#left-buble3',{ delay: HEIGHT/4, duration: HEIGHT/2, property:'opacity', start:0 });
+	scrollorama
+		.animate('#right-buble3',{ delay: HEIGHT/4, duration: HEIGHT/2, property:'left', start:WIDTH/2 })
+		.animate('#right-buble3',{ delay: HEIGHT/4, duration: HEIGHT/2, property:'opacity', start:0 });
+});
